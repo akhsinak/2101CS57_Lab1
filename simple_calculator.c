e31fec6bae7eb7a7c9636dce01e8a03d65d8c9a6
akhsinak <pstkanishka@gmail.com>
2022-07-27 21:57:23 +0530

    added printed statments to allow easy operations in the program

--- simple_calculator.c
+++ simple_calculator.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    int x, y;
+    float x, y;
     printf("Enter X:");
     scanf("%f", &x);
     printf("Enter Y:");
@@ -12,8 +12,10 @@ int main()
     printf("2 -> subtract\n");
     printf("3 -> multiply\n");
     printf("4 -> divide\n");
+    printf("Enter your option:");
     int status;
     scanf("%d", &status);
+
     switch (status)
     {
     case 1:
@@ -28,6 +30,8 @@ int main()
     case 4:
         printf("%f / %f = %f", x, y, x / y);
         break;
-        ` default : break;
+    default:
+        printf("Enter a valid option");
+        break;
     }
 }
