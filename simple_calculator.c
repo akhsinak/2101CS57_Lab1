5d096256b1bc25ad5bbf73af22047766e6ea0dd9
akhsinak <pstkanishka@gmail.com>
2022-07-27 21:42:32 +0530

    added addition feature

---
+++ simple_calculator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int x, y;
+    printf("Enter X:");
+    scanf("%f", &x);
+    printf("Enter Y:");
+    scanf("%f", &y);
+
+    printf("Choose the operation to perform:\n");
+    printf("1 -> add\n");
+    printf("2 -> subtract\n");
+    printf("3 -> multiply\n");
+    printf("4 -> divide\n");
+    int status;
+    scanf("%d", &status);
+    switch (status)
+    {
+    case 1:
+        printf("%f + %f = %f", x, y, x + y);
+        break;
+    default:
+        break;
+    }
+}
