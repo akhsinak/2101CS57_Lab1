62a7222eca0cee2747f07eadc97e55c6a93120d9
akhsinak <pstkanishka@gmail.com>
2022-07-27 22:24:41 +0530

    added insertion sort feature

--- sorter.c
+++ sorter.c
@@ -1,4 +1,25 @@
 #include <stdio.h>
+void sort_by_insertion_and_print(int arr[], int n)
+{
+    int p, slider, q;
+    for (p = 1; p < n; p++)
+    {
+        slider = arr[p];
+        q = p - 1;
+
+        while (q >= 0 && arr[q] > slider)
+        {
+            arr[q + 1] = arr[q];
+            q = q - 1;
+        }
+        arr[q + 1] = slider;
+    }
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
 int main()
 {
     int n;
@@ -25,6 +46,7 @@ int main()
     {
     case 1:
     {
+        sort_by_insertion_and_print(arr, n);
     }
     break;
 
